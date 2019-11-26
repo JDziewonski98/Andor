7277bb5b9d04eb70360da9148e0b1f386998a293
Anthony Cui <cuianthony@gmail.com>
2019-11-26 15:51:49 -0500

    remove physics square from game scene

--- examplegame/src/game.ts
+++ examplegame/src/game.ts
@@ -1,14 +1,7 @@
 import {Tile} from './tile';
 import { Tilemaps } from 'phaser';
 
-// const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
-//   active: false,
-//   visible: false,
-//   key: 'Game',
-// };
-
 export default class GameScene extends Phaser.Scene {
-  private square: Phaser.GameObjects.Rectangle & { body: Phaser.Physics.Arcade.Body };
   private weed: Phaser.GameObjects.Sprite;
   public tiles: Tile[] = [];
 
@@ -44,11 +37,8 @@ export default class GameScene extends Phaser.Scene {
     this.tiles[0].hero = this.weed;
     this.tiles[0].heroexist = true;
 
-    this.square = this.add.rectangle(400, 400, 100, 100, 0xFFFF00) as any;
-    // this.square.createBitmapMask(this.weed);
-    this.physics.add.existing(this.square);
-    this.square.setInteractive();
-    this.square.on('pointerdown', function (pointer) {
+    this.weed.setInteractive();
+    this.weed.on('pointerdown', function (pointer) {
         this.scene.start('Lobby');
     }, this);
   }
@@ -64,28 +54,5 @@ export default class GameScene extends Phaser.Scene {
 
 
   public update() {
-    const cursorKeys = this.input.keyboard.createCursorKeys();
- 
-    if (cursorKeys.up.isDown) {
-      this.square.body.setVelocityY(-1000);
-    } else if (cursorKeys.down.isDown) {
-      this.square.body.setVelocityY(600);
-    } else {
-        while (this.square.body.velocity.y != 0){
-          if (this.square.body.velocity.y > 0) {
-            this.square.body.setVelocityY(this.square.body.velocity.y - 1);
-          }
-          else {
-            this.square.body.setVelocityY(this.square.body.velocity.y + 1);
-          }
-        }
-    }
-    if (cursorKeys.right.isDown) {
-      this.square.body.setVelocityX(500);
-    } else if (cursorKeys.left.isDown) {
-      this.square.body.setVelocityX(-500);
-    } else {
-      this.square.body.setVelocityX(0);
-    }
   }
 }
