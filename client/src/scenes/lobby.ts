de1f6cfea6a66bf88abe26209420d6e80ff3c4d9
cmcclenen <cmcclenen@gmail.com>
2019-12-01 17:18:09 -0500

    Added hour bar and restricted movement to legal hours.

--- client/src/scenes/lobby.ts
+++ client/src/scenes/lobby.ts
@@ -5,26 +5,27 @@ export default class LobbyScene extends Phaser.Scene {
     private gameText;
     private scaleRatio = window.devicePixelRatio / 3;
     constructor() {
-        super({key: 'Lobby'});
+        super({ key: 'Lobby' });
     }
 
     public preload() {
-        this.load.image('beach','./assets/sample.jpg');
-        this.load.image('map','./assets/andor_map.jpg');
+        this.load.image('beach', './assets/sample.jpg');
+        this.load.image('map', './assets/andor_map.jpg');
         this.load.image('weed', './assets/8bit_herb.jpeg')
         this.load.image('desert', './assets/fantasydesert.jpg')
-        this.load.image('andordude','./assets/andordude.jpg')
-        this.load.image('backbutton','./assets/Pass.png')
-        this.load.image('playbutton','./assets/Play.png')
-        this.load.image('warriormale','./assets/warriormale.png')
-        this.load.image('magemale','./assets/magemale.png')
-        this.load.image('dwarfmale','./assets/dwarfmale.png')
-        this.load.image('archermale','./assets/archermale.png')
+        this.load.image('andordude', './assets/andordude.jpg')
+        this.load.image('backbutton', './assets/Pass.png')
+        this.load.image('playbutton', './assets/Play.png')
+        this.load.image('warriormale', './assets/warriormale.png')
+        this.load.image('magemale', './assets/magemale.png')
+        this.load.image('dwarfmale', './assets/dwarfmale.png')
+        this.load.image('archermale', './assets/archermale.png')
+        this.load.image('hourbar', './assets/hours.PNG')
     }
 
     public create() {
         var bg = this.add.image(500, 300, 'beach');
-        var style1 = { 
+        var style1 = {
             fontFamily: '"Roboto Condensed"',
             fontSize: "70px",
             shadow: {
@@ -35,32 +36,32 @@ export default class LobbyScene extends Phaser.Scene {
                 stroke: true,
                 fill: true
             },
-            color:"#4944A4"
-         }
-        this.welcomeText = this.add.text(500,200,"Welcome to Andor", style1).setOrigin(0.5)
-        
-        var style2 = { 
+            color: "#4944A4"
+        }
+        this.welcomeText = this.add.text(500, 200, "Welcome to Andor", style1).setOrigin(0.5)
+
+        var style2 = {
             fontFamily: '"Roboto Condensed"',
             fontSize: "40px",
             backgroundColor: "#f00"
-         }
-        this.weedText = this.add.text(500,300,"New", style2).setOrigin(0.5)
+        }
+        this.weedText = this.add.text(500, 300, "New", style2).setOrigin(0.5)
         this.weedText.setInteractive();
         this.weedText.on('pointerdown', function (pointer) {
             this.scene.start('Create');
         }, this);
 
-        this.gameText = this.add.text(500,400,"Join", style2).setOrigin(0.5)
+        this.gameText = this.add.text(500, 400, "Join", style2).setOrigin(0.5)
         this.gameText.setInteractive();
         this.gameText.on('pointerdown', function (pointer) {
             this.scene.start('Join');
         }, this);
 
-        this.gameText = this.add.text(500,500,"Load", style2).setOrigin(0.5)
+        this.gameText = this.add.text(500, 500, "Load", style2).setOrigin(0.5)
         this.gameText.setInteractive();
         this.gameText.on('pointerdown', function (pointer) {
             this.scene.start('Weed');
-        }, this); 
+        }, this);
     }
 
     public update() {
