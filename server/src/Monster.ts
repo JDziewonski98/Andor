e9243ffb3248526c9f04d2ea88e9335e703d4d8c
Anthony Cui <cuianthony@gmail.com>
2020-02-17 17:50:08 -0500

    added skeleton classes for Monster and Region

---
+++ server/src/Monster.ts
@@ -0,0 +1,5 @@
+class Monster {
+    constructor() {
+        
+    }
+}
---
+++ server/src/Region.ts
@@ -0,0 +1,54 @@
+class Region {
+    public adjacentRegions: Region[] = [];
+    nextRegion: Region;
+    public id: number;
+    // public heroexist: boolean = false;
+    public x: number;
+    public y: number;
+    // private graphic;
+    // public currHero: Hero;
+    // private fog: Fog;
+    private gold;
+    // private wineskins: Wineskin[] = [];
+    private hasWell;
+    private wellUsed: boolean = false;
+    private currMonster: Monster;
+    
+    constructor(id, x, y, hasWell, monster, nextRegion) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        // this.hero = null;
+        this.hasWell = hasWell;
+        // this.currHero = hero;
+        this.currMonster = monster;
+        this.nextRegion = nextRegion;
+    }
+
+    // public removeHero() {
+    //     currHero = null;
+    // }
+
+    public setGold(amount: number) {
+        this.gold = amount;
+    }
+    public getGold() {
+        return this.gold;
+    }
+
+    // public getFog() {
+    //     return this.fog;
+    // }
+
+    //deviates from design class diagrma
+    public setMonster(m: Monster) {
+        this.currMonster = m;
+    }
+    public getMonster() {
+        return this.currMonster;
+    }
+
+    // public addWineskin(w: Wineskin) {
+    //     this.wineskins.push(w);
+    // }
+}
