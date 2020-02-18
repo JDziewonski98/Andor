e868d4176b4e9161c2cc129b2fe837baefd542f6
JD908 <46234488+JD908@users.noreply.github.com>
2020-02-18 13:42:32 -0500

    shitty window dragging.

--- client/src/scenes/herowindow.ts
+++ client/src/scenes/herowindow.ts
@@ -4,15 +4,32 @@ export class HeroWindow extends Window {
         super(key, windowData);
     }
 
-
+ 
     protected initialize(){
         console.log("hero window", this)
         this.cameras.main.setBackgroundColor('0xa4sd44')
-        this.add.sprite(50, 50, 'weed').setOrigin(0.5);
+        var bg = this.add.image(0,0,'scrollbg').setOrigin(0.5)
+        var weed = this.add.sprite(50, 50, 'weed');
         this.add.text(50,100,'Gold: 5',{backgroundColor: 'fx00'})
         this.add.text(50,120,'Willpower: 7',{backgroundColor: 'fx00'})
         this.add.text(150,50,'Special ability text ....',{backgroundColor: 'fx00'})
         this.add.text(150,70,'Items ....',{backgroundColor: 'fx00'})
+        // weed.setInteractive()
+        // this.input.setDraggable(weed)
+        bg.setInteractive()
+        this.input.setDraggable(bg)
+        bg.on('drag', function (pointer, dragX, dragY) {
+            if (dragX < this.scene.parent.x - 10 && dragY < this.scene.parent.y - 10) {
+                this.scene.parent.x = this.scene.parent.x - 10;
+                this.scene.parent.y = this.scene.parent.y - 10;
+                this.scene.refresh()
+            }
+            else {
+                this.scene.parent.x = dragX;
+                this.scene.parent.y = dragY;
+                this.scene.refresh()
+            }
+        });
     }
 
 }
--- client/src/scenes/window.ts
+++ client/src/scenes/window.ts
@@ -6,22 +6,37 @@ export abstract class Window extends Phaser.Scene {
         super(key);
         this.windowData = windowData;
     } 
-
+ 
     create () {
         console.log("parent window create", this.windowData)
         this.parent = this.add.zone(this.windowData.x, this.windowData.y, this.windowData.width, this.windowData.height).setOrigin(0);
-        var bg = this.add.image(0,0,'scrollbg')
+        this.parent.setInteractive()
+        // this.input.setDraggable(this.parent)
         // this.input.setDraggable(bg)
         // bg.on('drag', function (pointer, dragX, dragY) {
         //     this.x = dragX;
         //     this.y = dragY;
         //     // demo.refresh()
         // });
-        this.initialize();
+        this.input.setDraggable(this.parent);
         this.cameras.main.setViewport(this.parent.x, this.parent.y, this.windowData.width, this.windowData.height);
         // this.input.keyboard.on('keydown_ESC',this.kill,this)
         // this.cameras.main.setBackgroundColor("6E8C97")
-        
+        //bg.setInteractive()
+        //this.input.setDraggable(bg)
+        // console.log(this.parent)
+        // this.parent.on('click', function(pointer){
+        //     console.log('works')
+        // })
+        // this.parent.on('drag', function (pointer, dragX, dragY) {
+        //     console.log(this.scene)
+            
+        //     this.x = dragX;
+        //     this.y = dragY;
+        //     //refresh()
+            
+        // });
+        this.initialize();
     }
 
     //press ESC to close windows
@@ -35,6 +50,12 @@ export abstract class Window extends Phaser.Scene {
         catch(e){
             console.log('something went wrong')
         }
+    }
+
+    refresh() {
+        this.cameras.main.setPosition(this.parent.x, this.parent.y);
+
+        this.scene.bringToTop();
     }
 
     protected abstract initialize(): void;
